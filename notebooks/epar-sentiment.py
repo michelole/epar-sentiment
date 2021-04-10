4bc0cb677d200ce81cdbb5efeb1ed7556c098402
Michel Oleynik <michel.oleynik@buildingminds.com>
2021-04-10 11:12:48 +0200

    Load the data and add basic descriptive analytics

--- notebooks/epar-sentiment.py
+++ notebooks/epar-sentiment.py
@@ -15,5 +15,43 @@
 
 # %% [md]
 # # Sentiment analysis of EPARs
-
+# %% [md]
+# Load the data.
 # %%
+import pandas as pd
+from pandas.core.arrays import base
+
+data = pd.read_excel("../data/sentences_with_sentiment.xlsx")
+print(data.dtypes)
+data.head()
+
+# %% [md]
+# Ensure that we have a single rating per sentence.
+# %%
+assert max(data['Positive'] + data['Negative'] + data['Neutral']) == 1
+assert min(data['Positive'] + data['Negative'] + data['Neutral']) == 1
+
+# %% [md]
+# ## Descriptive analytics
+# %% [md]
+# Count ratings.
+# %%
+positive = sum(data['Positive'])
+negative = sum(data['Negative'])
+neutral = sum(data['Neutral'])
+print(positive)
+print(negative)
+print(neutral)
+
+# %% [md]
+# Check length distribution.
+# %%
+lengths = data['Sentence'].str.len()
+print(lengths.describe())
+lengths.plot.box()
+
+# %% [md]
+# Set a majority baseline.
+# %%
+baseline_acc = max(positive, negative, neutral) / len(data)
+print(baseline_acc)
